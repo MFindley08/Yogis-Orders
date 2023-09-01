1389640cc3f46b0bff48edccee4da486dc41f5ab
M. Findley <123705549+MFindley08@users.noreply.github.com>
2023-09-01 12:24:07 -0400

    initial commit with React package installed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Yogi's Orders
+      </header>
+    </div>
+  );
+}
+
+export default App;
