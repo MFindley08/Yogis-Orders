653a01b8941bd1733fa9133db737db9e66ce8c66
M. Findley <123705549+MFindley08@users.noreply.github.com>
2023-09-02 17:47:53 -0400

    fixed babel plugin error

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      
+      </div>
     </div>
   );
 }
