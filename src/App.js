73d0eb24528b606951a55c4bd901cce517fc56fc
M. Findley <123705549+MFindley08@users.noreply.github.com>
2023-10-24 11:47:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
         <hero>
-          <img src={hero} className="App-hero" alt="yoga balanced in nature" />
+          <img
+            src={hero}
+            className="App-hero img-fluid"
+            alt="yoga balanced in nature hero"
+          />
           <h1 className="App-h1">Injury and Illness Recovery</h1>
         </hero>
       </div>
