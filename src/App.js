2ca9f32286faacc843431380f8cdedf7646bc449
M. Findley <123705549+MFindley08@users.noreply.github.com>
2023-11-09 10:37:21 -0400

    updated small logo, and made logos responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import hero from "./hero.png";
 import logo from "./logo.png";
+import alternate from "./logoSmall.png";
 import "./App.css";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <hero>
-          <h1 className="App-h1">Injury and Illness Recovery</h1>
-          <img
-            src={hero}
-            className="App-hero img-fluid"
-            alt="yoga balanced in nature hero"
-          />
-        </hero>
+    <Container>
+      <div className="App">
+        <div className="container">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <img
+              src={alternate}
+              className="Small-logo img-fluid"
+              alt="small logo"
+            />
+          </header>
+          <hero>
+            <h1 className="App-h1">Injury and Illness Recovery</h1>
+            <img
+              src={hero}
+              className="App-hero img-fluid"
+              alt="yoga balanced in nature hero"
+            />
+          </hero>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
